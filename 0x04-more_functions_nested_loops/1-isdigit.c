8389fa40b5dbf236b21fb52f18012e0b3b1e317b
Ojomo Sarah <ojomosarah@gmail.com>
2022-04-14 14:16:59 -0700

    task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _isdigit - checks if the no is b/n 0 to 9
+ * _isdigit - checkes if the no is b/n 0 to 9
  * @c: input no
- * Return: 1 if it is a number (0 to 9),0 in other case.
+ * Return: 1 if is a number (0 to 9),0 in other case.
  */
 int _isdigit(int c)
 {
